5339b93c7704033a34741c74e9608cff5117ee6c
dxdipon <131514884+dxdipon@users.noreply.github.com>
2023-04-22 12:59:55 +0600

    Update harshitethic.py

--- harshitethic.py
+++ harshitethic.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "ENTER YOUR API KEY HERE"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
@@ -15,7 +15,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "ENTER YOUR TELEGRAM TOKEN HERE"
+    tgkey = "6207072516:AAE1ezyvF2JaPE3gl2zkHspmLN5LmPuF7mg"
 else:
     tgkey = tgenv
 print(tgenv)
